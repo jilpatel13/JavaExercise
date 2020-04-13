36ca8059a30ced74acd938c47b26e271319730f7
jil <jilp131313@gmail.com>
2020-04-13 09:13:05 +0530

    initial commit

---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author jilpatel
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 8;
+        
+        double percent = 5.5;
+        
+        float percent1 = 5.5f;
+        
+        System.out.print(num + percent + percent1 );
+    }
+    
+}
